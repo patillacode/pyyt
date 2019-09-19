f176b919c615042ba9f86d36027b945feaf1bde2
PatillaCode <patillacode@gmail.com>
2019-09-20 00:32:05 +0200

    utf-8

--- pyyt.py
+++ pyyt.py
@@ -21,20 +21,20 @@ DOWNLOAD_FOLDER = '/tmp/'
 
 
 def run_command(command):
-        logging.debug('Running command `{0}`'.format(command))
-        p = subprocess.Popen(
-            command,
-            shell=True,
-            stderr=subprocess.PIPE,
-            stdout=subprocess.PIPE)
-        output, err = p.communicate()
+    logging.debug('Running command `{0}`'.format(command))
+    p = subprocess.Popen(
+        command,
+        shell=True,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE)
+    output, err = p.communicate()
 
-        if p.returncode != 0:
-            logging.error(err)
-            logging.error('Problem running command: {0}'.format(command))
-            raise
+    if p.returncode != 0:
+        logging.error(err)
+        logging.error('Problem running command: {0}'.format(command))
+        # raise
 
-        return output
+    return output
 
 
 def get_list_of_videos(playlist_url):
@@ -46,7 +46,7 @@ def get_list_of_videos(playlist_url):
 def download_as_audio(video_id):
     cmd = "cd {0}".format(DOWNLOAD_FOLDER)
     run_command(cmd)
-    cmd = ("youtube-dl -x --audio-format mp3 -o '{0}%(title)s-%(id)s.%(ext)s' "
+    cmd = ("youtube-dl -i -x --audio-format mp3 -o '{0}%(title)s-%(id)s.%(ext)s' "
            "http://www.youtube.com/watch?v={1}").format(
         DOWNLOAD_FOLDER,
         video_id)
@@ -68,7 +68,7 @@ def main():
             download_as_audio(video['id'])
             logging.info("#" * 100)
             logging.info("Audio for {} is ready at {}".format(
-                video['title'], DOWNLOAD_FOLDER))
+                video['title'].encode('utf-8'), DOWNLOAD_FOLDER))
             logging.info("#" * 100)
         logging.info("*" * 100)
         logging.info("All songs have been downloaded into {}".format(
