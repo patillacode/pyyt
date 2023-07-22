62087a11217e8ce0f088aa093da41f1624df3853
PatillaCode <patillacode@gmail.com>
2023-07-22 20:22:02 +0200

    aider: Refactored `pyyt.py` by adding comments (docstrings) and moving methods to other files (like `utils.py`) where necessary.
    # Context:
    USER: refactor the file `pyyt.py` by adding comments (docstrings) and moving methods to other files (like for example utils.py) where necesasry

--- pyyt.py
+++ pyyt.py
@@ -4,45 +4,13 @@ import pathlib
 import sys
 
 import yt_dlp
-
-ydl_opts = {
-    "format": "bestaudio/best",
-    "postprocessors": [
-        {
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "192",
-        },
-    ],
-    "outtmpl": f"{pathlib.Path().absolute()}/downloads/%(title)s.%(ext)s",
-    "extract_flat": "in_playlist",
-    "ignoreerrors": True,
-    "quiet": False,
-    "sleep_interval": 2,
-}
-ydl = yt_dlp.YoutubeDL(ydl_opts)
-
-
-def get_video_entries(playlist_url):
-    results = ydl.extract_info(
-        playlist_url,
-        download=False,  # We just want to extract the info
-    )
-
-    if "entries" in results:
-        entries = results["entries"]
-        return entries
-
-    print("Something went wrong, no entries in playlist...")
-    sys.exit(2)
-
-
-def download_and_metadata(video_url):
-    # TODO add metadata handling
-    ydl.download([video_url])
+from utils import get_video_entries, download_and_metadata
 
 
 def main():
+    """
+    Main function to run the pyyt script.
+    """
     print("Welcome to pyyt - Select your option:\n")
     print("1 - Download an entire playlist as audio\n")
     print("2 - Download a single video as audio\n")
---
+++ utils.py
@@ -0,0 +1,70 @@
+import sys
+
+import yt_dlp
+
+
+def get_video_entries(playlist_url):
+    """
+    Get the video entries from a YouTube playlist URL.
+
+    Args:
+        playlist_url (str): The URL of the YouTube playlist.
+
+    Returns:
+        list: A list of video entries.
+    """
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            },
+        ],
+        "outtmpl": f"{pathlib.Path().absolute()}/downloads/%(title)s.%(ext)s",
+        "extract_flat": "in_playlist",
+        "ignoreerrors": True,
+        "quiet": False,
+        "sleep_interval": 2,
+    }
+    ydl = yt_dlp.YoutubeDL(ydl_opts)
+
+    results = ydl.extract_info(
+        playlist_url,
+        download=False,  # We just want to extract the info
+    )
+
+    if "entries" in results:
+        entries = results["entries"]
+        return entries
+
+    print("Something went wrong, no entries in playlist...")
+    sys.exit(2)
+
+
+def download_and_metadata(video_url):
+    """
+    Download a video as audio and handle metadata.
+
+    Args:
+        video_url (str): The URL of the YouTube video.
+    """
+    # TODO add metadata handling
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            },
+        ],
+        "outtmpl": f"{pathlib.Path().absolute()}/downloads/%(title)s.%(ext)s",
+        "ignoreerrors": True,
+        "quiet": False,
+        "sleep_interval": 2,
+    }
+    ydl = yt_dlp.YoutubeDL(ydl_opts)
+
+    ydl.download([video_url])
