4ea3cfbb7a2b81b6483042252920225137913ec3
PatillaCode <patillacode@gmail.com>
2023-07-22 20:34:54 +0200

    aider: Use termcolor library for output messages, print welcome message and options in cyan, and print wrong option message in magenta.
    # Context:
    USER: make use of the termcolor library for the output messages, use vivid colors lige cyan and magenta

--- pyyt.py
+++ pyyt.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from termcolor import colored
 from utils import download_and_metadata, get_video_entries
 
 
@@ -7,14 +8,14 @@ def main():
     """
     Main function to run the pyyt script.
     """
-    print("Welcome to pyyt - Select your option:\n")
-    print("1 - Download an entire playlist as audio\n")
-    print("2 - Download a single video as audio\n")
+    print(colored("Welcome to pyyt - Select your option:\n", "cyan"))
+    print(colored("1 - Download an entire playlist as audio\n", "cyan"))
+    print(colored("2 - Download a single video as audio\n", "cyan"))
 
-    option = input("Write your selection: ")
+    option = input(colored("Write your selection: ", "cyan"))
 
     if option == "1":
-        playlist_url = input("Please insert the youtube playlist url: ")
+        playlist_url = input(colored("Please insert the youtube playlist url: ", "cyan"))
         video_entries = get_video_entries(playlist_url)
         video_list = [
             f'https://www.youtube.com/watch?v={video["id"]}' for video in video_entries
@@ -23,11 +24,11 @@ def main():
             download_and_metadata(video_url)
 
     elif option == "2":
-        video_url = input("Please insert the youtube video url: ")
+        video_url = input(colored("Please insert the youtube video url: ", "cyan"))
         download_and_metadata(video_url)
 
     else:
-        print("Wrong option. Cya!")
+        print(colored("Wrong option. Cya!", "magenta"))
 
 
 if __name__ == "__main__":
