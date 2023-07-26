87661cfd827254e0117b0be28d6c16b94b7c987c
PatillaCode <patillacode@gmail.com>
2023-07-26 20:54:14 +0200

    Update version in setup.py to "0.1.5".

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="pyyt-cli",
-    version="0.1.4",
+    version="0.1.5",
     description=(
         "Python script to download videos as audio from a given YouTube playlist/video"
     ),
